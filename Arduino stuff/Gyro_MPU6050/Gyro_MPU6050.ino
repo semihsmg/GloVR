679745cdb3849c73a8b8326f3d3e53ea5588db8a
Semih <semihsolmazgul@gmail.com>
2017-05-18 17:06:08 +0300

    Add files via upload

---
+++ Arduino stuff/Gyro_MPU6050/Gyro_MPU6050.ino
@@ -0,0 +1,207 @@
+// I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class using DMP (MotionApps v2.0)
+// 6/21/2012 by Jeff Rowberg <jeff@rowberg.net>
+// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
+
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+// class default I2C address is 0x68
+MPU6050 mpu;
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// Declare the pin(s) for the Button(s)
+int buttonPin1 = 12;
+int buttonPin2 = 11;
+int buttonPin3 = 10;
+int buttonPin4 = 9;
+int buttonPin5 = 8;
+
+// 0 open hand, 1 close hand
+int buttonState1;
+int buttonState2;
+int buttonState3;
+int buttonState4;
+int buttonState5;
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    // initialize device
+    //Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(13);
+    mpu.setYGyroOffset(44);
+    mpu.setZGyroOffset(-115);
+    mpu.setZAccelOffset(16472);
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        //Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        //Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        //Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+    }
+
+    // Activate the internal pull-up resistor
+    pinMode(buttonPin1, INPUT_PULLUP);
+    pinMode(buttonPin2, INPUT_PULLUP);
+    pinMode(buttonPin3, INPUT_PULLUP);
+    pinMode(buttonPin4, INPUT_PULLUP);
+    pinMode(buttonPin5, INPUT_PULLUP);
+}
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+  
+    //unsigned long StartTime = millis();
+  
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        //Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+        // Read the value of the input. It can either be 1 or 0
+        int buttonValue1 = digitalRead(buttonPin1);
+        int buttonValue2 = digitalRead(buttonPin2);
+        int buttonValue3 = digitalRead(buttonPin3);
+        int buttonValue4 = digitalRead(buttonPin4);
+        int buttonValue5 = digitalRead(buttonPin5);
+
+        // If button pushed, set value to 1. Else, set value to 0
+        if (buttonValue1 == LOW) buttonState1 = 1;
+        else buttonState1 = 0;
+        if (buttonValue2 == LOW) buttonState2 = 1;
+        else buttonState2 = 0;
+        if (buttonValue3 == LOW) buttonState3 = 1;
+        else buttonState3 = 0;
+        if (buttonValue4 == LOW) buttonState4 = 1;
+        else buttonState4 = 0;
+        if (buttonValue5 == LOW) buttonState5 = 1;
+        else buttonState5 = 0;
+    }
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            
+            //unsigned long CurrentTime = millis();
+            //unsigned long ElapsedTime = CurrentTime - StartTime;
+            //Serial.print(ElapsedTime);
+            //Serial.print(" ms >>> ");
+            
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[2] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(buttonState1);
+            Serial.print("\t");
+            Serial.print(buttonState2);
+            Serial.print("\t");
+            Serial.print(buttonState3);
+            Serial.print("\t");
+            Serial.print(buttonState4);
+            Serial.print("\t");
+            Serial.println(buttonState5);
+        #endif
+    }
+
